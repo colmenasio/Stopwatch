e0bd2044cdf11202d471b223905b2cb57fc192a9
Colmenero <a.colmenero.fernandez@gmail.com>
2024-01-28 22:17:23 +0100

    Countdown implementation

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn main() {
             borrowed_timer.start_timer();
             logic_module.set_is_running(true);
             logic_module.set_time_str("00:00:00".into());
-        } else if logic_module.get_is_running() {
+        } else {
             borrowed_timer.change_pause_state();
             logic_module.set_is_paused(!logic_module.get_is_paused());
         };
@@ -68,14 +68,44 @@ fn main() {
     // COUNTDOWN CALLBACKS //
 
     // l button
-    // r button
-    // add/substract time to the target time when inputing with the +- buttons
     let weak = main_window.as_weak();
     let simple_countdown = main_simple_countdown.clone();
 
-    main_window.global::<CLogic>().on_add_time_to_target(move |increment| {
+    main_window.global::<CLogic>().on_press_l_button(move || {
         let app = weak.unwrap();
         let logic_module = app.global::<CLogic>();
+        let mut borrowed_countdown = simple_countdown.borrow_mut();
+
+        if ! logic_module.get_is_running() {
+            borrowed_countdown.start_timer();
+            logic_module.set_is_running(true);
+            logic_module.set_time_str("00:00:00".into());
+        } else {
+            borrowed_countdown.change_pause_state();
+            logic_module.set_is_paused(!logic_module.get_is_paused());
+        };
+    });
+
+    // r button
+    let weak = main_window.as_weak();
+
+    main_window.global::<CLogic>().on_press_r_button(move || {
+        let app = weak.unwrap();
+        let logic_module = app.global::<CLogic>();
+
+        if logic_module.get_is_running() {
+            logic_module.set_is_running(false);
+            logic_module.set_is_paused(false);
+            logic_module.set_is_finished(false);
+
+        };
+    });
+
+
+    // add or substract time to the target time when inputing with the +- buttons
+    let simple_countdown = main_simple_countdown.clone();
+
+    main_window.global::<CLogic>().on_add_time_to_target(move |increment| {
         let mut borrowed_countdown = simple_countdown.borrow_mut();
 
         borrowed_countdown.add_time_to_target(increment)
@@ -89,10 +119,14 @@ fn main() {
     main_window.global::<CLogic>().on_update_time(move || {
         let app = weak.unwrap();
         let logic_module = app.global::<CLogic>();
-        let borrowed_countdown = simple_countdown.borrow_mut();
+        let borrowed_countdown = simple_countdown.borrow();
 
         // TODO add logic to determine which thing to update
-        logic_module.set_inactive_str(borrowed_countdown.get_display().into());
+        logic_module.set_time_str(borrowed_countdown.get_display().into());
+        logic_module.set_inactive_str(borrowed_countdown.get_target_display().into());
+        if logic_module.get_is_running() && borrowed_countdown.is_finished() {
+            logic_module.set_is_finished(true);
+        }
     });
 
     // MAIN APP LOOP //
--- src/simple_countdown.rs
+++ src/simple_countdown.rs
@@ -19,19 +19,60 @@ impl SimpleCountdown {
             pause_start_time: time::Instant::now(),
             is_paused: false,
             total_paused_time: 0,
-            target: 0
+            target: 0,
         }
     }
 
     pub fn add_time_to_target(&mut self, increment: i32){
         self.target = min(self.target.saturating_add_signed(increment as i64), MAX_TARGET);
     }
-    pub fn get_display(&self) ->String{
-        //let seconds_elapsed = if self.is_paused{
-        //    (self.pause_start_time-self.start_time).as_secs()-self.total_paused_time
-        //} else {
-        //    (time::Instant::now()-self.start_time).as_secs()-self.total_paused_time
-        //};
+
+    pub fn start_timer(& mut self){
+        self.start_time = time::Instant::now();
+        self.is_paused = false;
+        self.total_paused_time = 0;
+    }
+
+    pub fn change_pause_state(& mut self){
+        if self.is_paused {
+            self.unpause_timer();
+        } else {
+            self.pause_timer();
+        }
+    }
+
+    fn pause_timer(& mut self){
+        self.pause_start_time = time::Instant::now();
+        self.is_paused = true;
+    }
+
+    fn unpause_timer(& mut self){
+        self.total_paused_time += self.pause_start_time.elapsed().as_secs();
+        self.is_paused = false;
+    }
+
+    fn get_elapsed_seconds(& self) -> u64{
+        if self.is_paused {
+            (self.pause_start_time-self.start_time).as_secs()-self.total_paused_time
+        } else {
+            (time::Instant::now()-self.start_time).as_secs()-self.total_paused_time
+        }
+    }
+
+    pub fn is_finished(&self) -> bool {
+        self.get_elapsed_seconds() >= self.target
+    }
+
+    pub fn get_display(&self) -> String{
+        let seconds_elapsed = self.get_elapsed_seconds();
+        if seconds_elapsed >= self.target {
+            "00:00:00".to_string()
+        } else {
+            common::stringify_seconds(self.target-seconds_elapsed)
+        }
+    }
+
+    pub fn get_target_display(&self) -> String{
         common::stringify_seconds(self.target)
     }
 }
